bd561a0ed85735b1c5e50b0dcc55f16941d216fd
Dylan <klj40702@gmail.com>
2023-12-24 12:35:32 +0800

    update site content

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://astro-paper.pages.dev/",
+  author: "Dylan",
+  desc: "一個簡單的部落格，大多數是記錄自己在前端工作上遇到的問題、或是一些比較零散的技術問題。",
+  title: "Dylan's Blog",
+  ogImage: "og-img.png",
   lightAndDarkMode: true,
   postPerPage: 10,
   recentPostNum: 5,
@@ -26,7 +26,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/Dylan0916",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -44,7 +44,7 @@ export const SOCIALS: SocialObjects = [
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/rd0916/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
